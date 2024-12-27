8959da36241eca129338876d3e1e9ed4f955cf03
Suhaib omar <suhaib.omar.2003ise@gmail.com>
2024-12-27 21:50:59 +0300

    add product using anither script file

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -1,40 +1,4 @@
-const products=[{
-    image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name:' Black and Gray Athletic Cotton Socks - 6 Pairs',
-    priceCents:1090,
-    rating:{
-        stars:4.5,
-        count:87
-    },
 
-
-},{
-    image:'images/products/intermediate-composite-basketball.jpg',
-    name:'Intermediate Size Basketball',
-    priceCents:2095,
-    rating:{
-        stars:4,
-        count:127
-    },
-    priceCents:2095,
-
-},{
-    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name:'Adults Plain Cotton T-Shirt - 2 Pack',
-    priceCents:799,
-    rating:{
-        stars:4.5,
-        count:56
-    },
-},{
-  image:'images/products/black-2-slot-toaster.jpg',
-  name:'2 Slot Toaster - Black',
-  priceCents:1899,
-  rating:{
-    stars:5,
-    count:2197
-  },
-}];
 
 let data='';
 products.forEach((product)=>{
